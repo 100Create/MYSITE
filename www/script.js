33618a6c81a8a55bdb26d57a552edbc02acb2632
Coder696969 <112969644+100Create@users.noreply.github.com>
2024-05-26 13:05:22 +0100

    removed bloat

--- www/script.js
+++ www/script.js
@@ -9,14 +9,4 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => {
             console.error('Error fetching stock price:', error);
         });
-
-    // Fetch networth
-    fetch('networth.php')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('networth-info').textContent = `Player 1 networth: $${data.player1_networth}, Player 2 networth: $${data.player2_networth}`;
-        })
-        .catch(error => {
-            console.error('Error fetching networth:', error);
-        });
 });
